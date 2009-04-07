ccca9f0e3f18c5fcf67154a334629ccc3c714597
unknown <Nathan@.(none)>
2009-04-06 23:33:17 -0400

    working towards a better system of tyling and having a square grid mapping.

--- hexmap.js
+++ hexmap.js
@@ -1,39 +1,65 @@
-var svg,hx0;
+var svg,hex0;
 var point0;
 var n = 1;
 
+var board = [];
+//square array representation
+/*
+ *  |------------------|
+ *  |  	|   | 0 |   |  |
+ *  |------------------|
+ *  |  	| 5 |  	| 1 |  |
+ *  |------------------|
+ *  |  	|   | X	|   |  |
+ *  |------------------|
+ *  |  	| 4 |  	| 2 |  |
+ *  |------------------|
+ *  |  	|   | 3	|   |  |
+ *  |------------------|
+ */
+-*
 function init()
 {
-    hx0=$("#hx0")[0];
     svg = $("#svg")[0];
+    hex0=$("#hex0")[0];
+
     //  point0 = triangle.points.getItem(0);
-    console.log("Started: ", hx0, svg);
-    
-    for(var y= 0; y < 1000; y+=92) {
-	buildRow(0,y);
-	buildRow(156/2 + 1,y+46);
-    }
+    console.log("Started: ", hex0, svg);
+    //buildMap()
 }
 
-function buildRow(x,y) {
+function buildMap() {
     //104,90
-    for(var i=0; i < 16; i++){
-	cloneAndAdd(158*i+x,y);
+    for(var j = 0; j < 25; j++) {
+	var y = j*46;
+	var xbase = j%2 ? 156/2 : 0;
+	for(var i=0; i < 16; i++){
+	    var x = 158*i + xbase;
+	    var hex = cloneAndAdd(x,y);
+	    board[i,j]=hex;
+	}
     }
-
 }
 
 function cloneAndAdd(transX, transY)
-{
+{	      
   var xform = svg.createSVGTransform();
   xform.setTranslate(transX,transY);
 
-  var hx = hx0.cloneNode(true);
-  hx.id = "hx" + n++;
-  hx.style.display="block";
-  hx.transform.baseVal.appendItem(xform);
-  //hx.addEventListener("mouseover",
+  var hex = hex0.cloneNode(true);
+  hex.id = "hex" + n++;
+  hex.style.display="block";
+  hex.neighbors = new Array(6);
+  hex.transform.baseVal.appendItem(xform);
+  //hex.addEventListener("mouseover",
   //function(evt) { console.log("Clicked: ",evt.target.id, evt) },
   //		      false)
-  svg.appendChild(hx);
+  svg.appendChild(hex);
+  return hex;
+}
+
+
+function blink(id) {
+    $("#" + id).addClass("red");
+    window.setTimeout(function() {   $("#" + id).removeClass("red"); },0);
 }
